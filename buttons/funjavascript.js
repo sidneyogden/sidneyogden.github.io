8008a1c07d3b4b2728d119e759c0839945086a57
smorgan93 <msidney1293@gmail.com>
2023-11-09 18:14:48 -0600

    New mostly JS button

--- buttons/funjavascript.js
+++ buttons/funjavascript.js
@@ -1,8 +1,25 @@
+function createFirstButton() {
+        const myButton = document.createElement("button");
+        myButton.setAttribute('onclick', 'moreButton()');
+
+        const newImage = document.createElement("img");
+        newImage.src = "funnycow.jpeg";
+
+        myButton.appendChild(newImage);
+
+        document.getElementById('btn-container').appendChild(myButton);
+}
+
 function moreButton() {
         const node = document.querySelector('#btn-container > button');
         const clone = node.cloneNode(true);
         document.getElementById('btn-container').appendChild(clone);
 }
+
+window.onload = function() {
+        createFirstButton();
+}
+
 // function getRandomColor() {
 //         const currentColor = document.getElementById("myButton").style.backgroundColor;
 
